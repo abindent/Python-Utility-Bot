5484a0ff4e04db902a3dc5d24fb2220f8258853a
Sinchan Maitra <86192575+abindent@users.noreply.github.com>
2022-01-16 20:55:11 +0530

    Update server.py

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ def home():
     return '''<a style='text-decoration: none; color: green;' href='https://discord.io/OpenSourceGames'><button">DISCORD SERVER</button></a>'''
 
 def run():
-  app.run(host='0.0.0.0',port=3000)
+  app.run(host='0.0.0.0',port=os.environ.get("PORT", 5000))
 
 def keep_alive():
     t = Thread(target=run)
