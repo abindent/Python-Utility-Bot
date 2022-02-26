a596e81e823e290bb53c6302058e197ab54d88f0
Sinchan Maitra <86192575+abindent@users.noreply.github.com>
2022-02-26 12:33:59 +0530

    Update slash.py

--- bot/cogs/slash.py
+++ bot/cogs/slash.py
@@ -62,8 +62,7 @@ class DropDown(nextcord.ui.Select):
 class DropDownView(nextcord.ui.View):
     def __init__(self):
         super().__init__()
- 
-self.add_item(DropDown())
+        self.add_item(DropDown())
           
 class Slash(commands.Cog):
     def __init__(self, bot):
