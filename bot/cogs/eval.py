063558d4f4f0aa77c8f8009a17a15946f82c2a40
Sinchan Maitra <86192575+abindent@users.noreply.github.com>
2022-03-02 20:34:36 +0530

    Update eval.py

--- bot/cogs/eval.py
+++ bot/cogs/eval.py
@@ -14,7 +14,7 @@ class Eval(commands.Cog):
         self.bot = bot
         
     async def prepare(self, string):
-        arr = await string.strip("```").replace("py\n", "").replace("python\n", "").split("\n")
+        arr =  string.strip("```").replace("py\n", "").replace("python\n", "").split("\n")
         if not arr[::-1][0].replace(" ", "").startswith("return"):
             arr[len(arr) - 1] = "return " + arr[::-1][0]
         return "".join(f"\n\t{i}" for i in arr)
