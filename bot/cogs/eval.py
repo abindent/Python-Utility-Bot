d24a14665fb779ab82aaa1805a386b2d453badef
Sinchan Maitra <86192575+abindent@users.noreply.github.com>
2022-03-02 20:27:34 +0530

    Update eval.py

--- bot/cogs/eval.py
+++ bot/cogs/eval.py
@@ -25,8 +25,8 @@ class Eval(commands.Cog):
         
         
     @commands.command(name="excecute", description="Evaluates the given python code", aliases=['eval', 'exec', 'evaluate'])
-    async def excecute(self, ctx: commands.Context, *, code):
-           code = self.prepare(code.replace("-s", ""))
+    async def excecute(self, ctx: commands.Context, *, code: str):
+           code = self.prepare(code)
            view = DelBtn()
            stdout = io.StringIO()
            with contextlib.redirect_stdout(stdout):
