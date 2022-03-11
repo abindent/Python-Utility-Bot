ed589bc9b1ff7abb104f10e7ac70d2b10e78c3b3
Sinchan Maitra <86192575+abindent@users.noreply.github.com>
2022-03-11 15:39:40 +0530

    Update _sub-commands.py

--- bot/cogs/_sub-commands.py
+++ bot/cogs/_sub-commands.py
@@ -12,8 +12,8 @@ class Groups(commands.Cog):
     async def on_ready(self):
         print(f"{self.__class__.__name__} Cog has been loaded\n-----")
 
-""" The commmand group below is a group of commands having sub-commands.We often use [prefix][main_command] [subcommand(always optional)] [url] this type of things in bots.
-For maintainance and a lot of issues we usually split the funtionality of the parent command into different child subcommands.You can use or not but you can split them."""
+    """ The commmand group below is a group of commands having sub-commands.We often use [prefix][main_command] [subcommand(always optional)] [url] this type of things in bots.
+     For maintainance and a lot of issues we usually split the funtionality of the parent command into different child subcommands.You can use or not but you can split them."""
         
     @commands.group(invoke_without_command=True)
     async def first(self, ctx):
@@ -22,22 +22,28 @@ For maintainance and a lot of issues we usually split the funtionality of the pa
 
     @first.group(invoke_without_command=True)
     async def second(self, ctx, channel: nextcord.TextChannel=None):
-         """ This code will be excecuted when using this child subcommand with parent command. """
-            
-        # Getiing the channel id if  provided 
-        if channel_id not None:
-     
-            # Sending the message to the mentioned channel
-            await channel.send(
+        """ This code will be excecuted when using this child subcommand with parent command. """
+
+        if channel is None:
+            await ctx.send(
+                "Hey! This is a message from me the bot. Bet you didn't see who ran the command?",
+                delete_after=15,
+            )
+        #Sending the message to the mentioned channel
+        else:
+          await channel.send(
                 "Hey! This is a message from me the bot. Bet you didn't see who ran the command?",
                 delete_after=15,
             )
 
     @second.command()
-    async def third(self, ctx, channelId=None):
-         """ This code will be excecuted when using this child subcommand with the parent command. """
+    async def third(self, ctx, channel: nextcord.TextChannel=None):
+        """ This code will be excecuted when using this child subcommand with the parent command. """
+        if channel:
+            await channel.send("Hey! Did this come through clearly?")
          # Sending a message to the author
-        await ctx.message.author.send("Hey! Did this come through clearly?")
+        else: 
+            await ctx.message.author.send("Hey! Did this come through clearly?")
 
 
 def setup(bot):
