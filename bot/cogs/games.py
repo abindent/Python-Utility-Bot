fe09b05dd7b368aac568041e7f84702fb9bdbdd5
Sinchan Maitra <86192575+abindent@users.noreply.github.com>
2022-03-16 17:41:26 +0530

    Update games.py

--- bot/cogs/games.py
+++ bot/cogs/games.py
@@ -22,12 +22,42 @@ class Games(commands.Cog):
 
         else:        
            processed_as_guess = await process_message_as_guess(self.bot, message)
+        
 
     @commands.command(name="tictactoe", description="Playing TicTacToe.....")
     async def tic(self, ctx: commands.Context):
         """Starts a tic-tac-toe game with yourself."""
         await ctx.send('Tic Tac Toe: X goes first', view=TicTacToe())
 
+
+    @commands.group(invoke_without_command=True, name="wordle", description="Play wordle with me.")
+    async def wordle_normal(self, ctx):
+       pass
+
+    @wordle_normal.command(name="random", description="Play a random game of Wordle")
+    async def slash_play_random_normal(self, ctx):
+        embed = generate_puzzle_embed(self.bot, ctx.author, random_puzzle_id())
+        await ctx.send(embed=embed)
+
+    @wordle_normal.command(name="id", description="Play a game of Wordle by its ID")
+    async def slash_play_id_normal(
+        self,
+        ctx,
+        puzzle_id: int,
+    ):
+        embed = generate_puzzle_embed(self.bot, ctx.author, puzzle_id)
+        await ctx.send(embed=embed)
+
+    @wordle_normal.command(name="daily", description="Play the daily game of Wordle")
+    async def slash_play_daily_normal(self, ctx):
+        embed = generate_puzzle_embed(self.bot, ctx.author, daily_puzzle_id())
+        await ctx.send(embed=embed)
+
+    @wordle_normal.command(name="info", description="Wordle Info")
+    async def slash_info_normal(self, ctx):
+        await ctx.send(embed=generate_info_embed())
+
+    
     @nextcord.slash_command(name="playwordle", description="Play wordle with me.")
     async def wordle(self, interaction: nextcord.Interaction):
        pass
