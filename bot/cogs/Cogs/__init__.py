0038e8b8253c537adcdaac2b7a572f8416fcf113
Sinchan Maitra <86192575+abindent@users.noreply.github.com>
2022-04-28 16:39:39 +0530

    Bug Fixes in cog manager code.

--- bot/cogs/Cogs/__init__.py
+++ bot/cogs/Cogs/__init__.py
@@ -10,18 +10,21 @@ class CogSetup(commands.Cog, name="Extension Setup", description="Load, Unload o
     
     # Load Command
      @commands.command(name="load", description="Load the cogs.", usage="<cog_name or extension_name>")
+     @commands.is_owner()
      async def load(self, ctx, extensions):
         self.bot.load_extension(f"cogs.{extensions}") 
         await ctx.send("Loaded Cogs")
 
     # Unload Comamnd
      @commands.command(name="unload", description="Unload the cogs.", usage="<cog_name or extension_name>")
+     @commands.is_owner()
      async def unload(self, ctx, extensions):
         self.bot.unload_extension(f"cogs.{extensions}")
         await ctx.send("Unloaded Cogs")
 
     # Reload Command
      @commands.command(name="reload", description="Reload the cogs.", usage="<cog_name or extension_name>")
+     @commands.is_owner()
      async def reload(self, ctx, extensions):
         self.bot.reload_extension(f"cogs.{extensions}")
         await ctx.send("Reloaded Cogs")
